544f7eac9aa12d227977563b53371cbcb89d2b47
Daniela Alves Barbosa <48739735+daniela-ab@users.noreply.github.com>
2026-03-31 21:04:53 -0300

    Correção do javascript

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function abrirLightbox(imagem) {
   const lightbox = document.getElementById("lightbox");
   const img = document.getElementById("lightbox-img");
+
   img.src = imagem;
   lightbox.style.display = "flex";
+
   setTimeout(() => lightbox.classList.add("show"), 10);
+
+  document.body.style.overflow = "hidden";
 }
 
 function fecharLightbox() {
   const lightbox = document.getElementById("lightbox");
+
   lightbox.classList.remove("show");
+
   setTimeout(() => {
     lightbox.style.display = "none";
   }, 300);
+
+  document.body.style.overflow = "auto";
 }
